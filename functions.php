7c23f71e2a50ffd21c7fb8b84e078b6e24648b58
Christine <cj3wilso@gmail.com>
2019-10-26 15:13:12 -0400

    more changes

--- functions.php
+++ functions.php
@@ -171,11 +171,9 @@ function create_git_project($projectdir,$shfile){
 
 function create_database($projectdir,$shfile){
 	$command_with_parameters = "/var/www/\"${shfile}\".sh \"${projectdir}\"";
-	echo $command_with_parameters;
-	return true;
 	$output = $return = "";
-	$exec = exec("${command_with_parameters}", $output, $return,true);
-	display_errors($exec, $output, $return, 'Create WordPress Database');
+	$exec = exec("${command_with_parameters}", $output, $return);
+	display_errors($exec, $output, $return, 'Create WordPress Database',true);
 }
 
 function create_subdomain($projectdir,$projecturl,$stage){
