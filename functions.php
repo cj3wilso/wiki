02ca89f20adf7df9e12eabdaf156c0d6184c0272
Christine <cj3wilso@gmail.com>
2019-10-26 15:58:50 -0400

    try different way of checking for directory

--- functions.php
+++ functions.php
@@ -227,7 +227,8 @@ function delete_project(){
 				/*
 				* REMOVE DATABASE IF WORDPRESS FOUND 
 				*/
-				if ( is_dir( "/var/www/$projectdir/public_html/wp-content/" ) ) {
+				if ($dir = opendir("/var/www/$projectdir/public_html/wp-content")){ 
+				//if ( is_dir( "/var/www/$projectdir/public_html/wp-content/" ) ) {
 					$command_with_parameters = "/var/www/database-delete.sh \"${projectdir}\"";
 					$output = $return = "";
 					$exec = exec("${command_with_parameters}", $output, $return);
