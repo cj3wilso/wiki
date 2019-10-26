5cc74fa40e5c8cf5b37d9936096f3b53aa6ff02f
Christine <cj3wilso@gmail.com>
2019-10-26 15:09:45 -0400

    semi colon

--- functions.php
+++ functions.php
@@ -171,7 +171,7 @@ function create_git_project($projectdir,$shfile){
 
 function create_database($projectdir,$shfile){
 	$command_with_parameters = "/var/www/\"${shfile}\.sh \"${projectdir}\"";
-	echo $command_with_parameters
+	echo $command_with_parameters;
 	return true;
 	$output = $return = "";
 	$exec = exec("${command_with_parameters}", $output, $return);
